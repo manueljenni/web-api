17ef2b9ecb30432dd58d474e8c7b7abde2c5513a
legalnoah <manuelnoahjenni@gmail.com>
2022-07-01 22:37:25 +0200

    Fix queries for article

--- src/main/java/com/manueljenni/api/Repo/ArticleRepo.java
+++ src/main/java/com/manueljenni/api/Repo/ArticleRepo.java
@@ -12,24 +12,24 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleRepo extends JpaRepository<Article, Long> {
 
-  @Query(value = "SELECT * FROM article WHERE active = 1 ORDER BY publication_date DESC",
+  @Query(value = "SELECT id, title, subtitle, link, summary, publication_date as publicationDate, content, category, tags, image FROM article WHERE active = TRUE ORDER BY publication_date DESC",
       nativeQuery = true
   )
   List<ArticleResult> findAllArticles();
 
   @Query(value = "SELECT id, title, subtitle, summary, link, publication_date as publicationDate, image, category " +
-      "FROM article WHERE active = 1 ORDER BY publication_date DESC",
+      "FROM article WHERE active = TRUE ORDER BY publication_date DESC",
       nativeQuery = true
   )
   List<ArticleSummaryResult> findAllArticlesSummary();
 
   @Query(value = "SELECT id, title, subtitle, summary, link, publication_date as publicationDate, image, category " +
-      "FROM article WHERE active = 1 AND category = ? ORDER BY publication_date DESC",
+      "FROM article WHERE active = TRUE AND category = ? ORDER BY publication_date DESC",
       nativeQuery = true
   )
   List<ArticleSummaryResult> findArticleSummaryByCategory(String category);
 
-  @Query(value = "SELECT * FROM article WHERE id = ? ORDER BY publication_date DESC",
+  @Query(value = "SELECT id, title, subtitle, link, summary, publication_date as publicationDate, content, category, tags, image FROM article WHERE active = TRUE ORDER BY publication_date DESC",
       nativeQuery = true
   )
   ArticleResult findArticleById(Long id);
